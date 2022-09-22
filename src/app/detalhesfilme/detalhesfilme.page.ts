f580e5c01c085304d17defb0d6e4c063f09466e6
Aluno <aluno@devisate.local>
2022-09-22 19:07:13 -0300

    terminar

---
+++ src/app/detalhesfilme/detalhesfilme.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhesfilme',
+  templateUrl: './detalhesfilme.page.html',
+  styleUrls: ['./detalhesfilme.page.scss'],
+})
+export class DetalhesfilmePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
